5cb76a3d91494212c010a4e56d0e612b52751c7e
marianarizzo <68084839+marianarizzo@users.noreply.github.com>
2026-04-16 16:02:22 -0300

    Add static file serving with FastAPI

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
